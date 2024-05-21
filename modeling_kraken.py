6c5963a44490ed5ec2e1ac6d5bc66e984dd86c87
dgolchin <david.golchinfar@h-brs.de>
2024-05-21 07:44:13 +0200

    Add files via upload

--- modeling_kraken.py
+++ modeling_kraken.py
@@ -8,12 +8,29 @@ class KrakenForCausalLM(PreTrainedModel):
 
     def __init__(self, config):
         super().__init__(config)
-        self.tokenizers = {key: AutoTokenizer.from_pretrained(name) for key, name in config.config_dict['tokenizers'].items()}
-        self.models = {key: AutoModelForCausalLM.from_pretrained(name, trust_remote_code=True) for key, name in config.config_dict['models'].items()}
-        self.router_model = AutoModelForSequenceClassification.from_pretrained(config.config_dict['router'], trust_remote_code=True)
-        self.tokenizer = AutoTokenizer.from_pretrained(config.config_dict['router'], trust_remote_code=True)
+        self.tokenizers = {key: AutoTokenizer.from_pretrained(name, device_map="auto") for key, name in config.config_dict['tokenizers'].items()}
+        self.models = self.load_expert_models(config.config_dict['models'], config.config_dict['quantization'])
+        self.router_model = AutoModelForSequenceClassification.from_pretrained(config.config_dict['router'], trust_remote_code=True,device_map="auto")
+        self.tokenizer = AutoTokenizer.from_pretrained(config.config_dict['router'], trust_remote_code=True,device_map="auto")
         self.router = TextClassificationPipeline(model=self.router_model, tokenizer=self.tokenizer)
         self.models_indices = config.config_dict['class_indices']
+
+    def load_expert_models(self, models_dict, quantization_dict):
+        models = {}
+        for key, name in models_dict.items():
+            quantization = quantization_dict.get(key)
+            if quantization == "8bit":
+                models[key] = AutoModelForCausalLM.from_pretrained(name, trust_remote_code=True, device_map="auto", load_in_8bit=True)
+            elif quantization == "4bit":
+                models[key] = AutoModelForCausalLM.from_pretrained(name, trust_remote_code=True, device_map="auto", load_in_4bit=True)
+            elif quantization == "awq":
+                models[key] = self.load_awq_model(name)
+            else:
+                models[key] = AutoModelForCausalLM.from_pretrained(name, trust_remote_code=True, device_map="auto")
+        return models
+
+    def load_awq_model(self, name):
+        return AutoModelForCausalLM.from_pretrained(name, trust_remote_code=True, device_map="auto")
 
     def tokenize_inputs(self, text, model_key):
         return self.tokenizers[model_key](text, return_tensors="pt")
@@ -21,19 +38,22 @@ class KrakenForCausalLM(PreTrainedModel):
     def determine_model(self, text):
         prediction = self.router(text)[0]["label"]
         model_decision_index = self.models_indices[prediction]
-        model_keys = ['expert1', 'expert2', 'expert3', 'expert4']
+        model_keys = ['expert1', 'expert2', 'expert3', 'expert4','expert5']
         return model_keys[model_decision_index]
     
     def expert_tokenizer(self, text):
         model_key = self.determine_model(text)
         return self.tokenizers[model_key]
 
+
     def generate(self, input_ids, **generate_kwargs):
         # Tokenize the input_ids
         text = self.tokenizer.batch_decode(input_ids, skip_special_tokens=False)[0]
 
         msgs = tokenizer_template_switch.recover_chat_messages(text, self.tokenizer)
-
+        if msgs and msgs[0]['role'] == 'system' and msgs[0]['content']=='<|im_start|>system':
+            # Delete the first element
+            msgs.pop(0)  
         # Check if the last element has the role 'assistant'
         if msgs and msgs[-1]['role'] == 'assistant':
             # Delete the last element
@@ -41,15 +61,18 @@ class KrakenForCausalLM(PreTrainedModel):
 
         # Determine the model key using the existing routing logic
         model_key = self.determine_model(text)
-        
+        # Show the routing result
+        print(f"Choosing {model_key} ..")
         # Retrieve the model from the dictionary
         model = self.models[model_key]
 
         mod_txt = self.tokenizers[model_key].apply_chat_template(msgs, tokenize=False, add_generation_prompt=True)
-
+        current_device = input_ids.device if isinstance(input_ids, torch.Tensor) else 'cpu'
         # Tokenize accordingly to the best model
-        tok = self.tokenizers[model_key](mod_txt, return_tensors="pt").input_ids  
+        tok = self.tokenizers[model_key](mod_txt, return_tensors="pt").input_ids.to(current_device)  
         
         # Generate text using the retrieved model
         return model.generate(tok, **generate_kwargs)
     
+
+      
